a560a68bd38fcd6d45c2ce76ab612b28ec4cbe13
Nathália <nathaliaveneziano@icloud.com>
2021-04-17 16:57:57 -0300

    ReactJS - Classes e Funções

--- reactjs/react-102/src/App.js
+++ reactjs/react-102/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Forma muito usada até pouco tempo
+/* class App extends React.Component {
+  render() {
+    return <h1>Testando 1, 2, 3...</h1>;
+  }
+} */
+
+// Forma mais usada atualmente
+/* function App() {
+  return <h1>Testando 4, 5, 6...</h1>;
+} */
+
+// Constante que recebe uma função (Arrow Function)
+/* let App = () => {
+  return <h1>Testando 7, 8, 9...</h1>;
+} */
+
+// Arrow Function com retorno direto, pouco usado e para algo direto
+let App = () => <h1>Testando 10, 11, 12...</h1>;
 
 export default App;
