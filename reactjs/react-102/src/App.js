226d14f68680953d43370666924eaead9f666009
Nathália <nathaliaveneziano@icloud.com>
2021-04-17 19:10:09 -0300

    ReactJS - useState

--- reactjs/react-102/src/App.js
+++ reactjs/react-102/src/App.js
@@ -1,33 +1,17 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Site = styled.div`
-	width: 400px;
-	height: 400px;
-	background: #00ff00;
-`;
-
-const Botao = styled.button`
-	font-size: 19px;
-	padding: 10px 15px;
-	border: 3px dashed #ff0000;
-	color: #ff0000;
-	background: #ffff;
-	margin: 5px;
-	border-radius: 5px;
-`;
-
-const BotaoPequeno = styled(Botao)`
-	font-size: 16px;
-	padding: 5px 10px;
-`;
+import React, { useState } from 'react';
 
 function App() {
+	const [contagem, setContagem] = useState(0);
+
+	const botaoAction = () => {
+		setContagem(contagem + 1);
+	};
+
 	return (
-		<Site>
-			<Botao>Clique aqui</Botao>
-			<BotaoPequeno>Clique aqui</BotaoPequeno>
-		</Site>
+		<>
+			<div>{contagem} vezes</div>
+			<button onClick={botaoAction}>Clique para aumentar</button>
+		</>
 	);
 }
 
