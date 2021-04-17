e528f066c213675e8a69f012d77411cf5cdd5b7a
Nathália <nathaliaveneziano@icloud.com>
2021-04-17 17:13:43 -0300

    ReactJS - Entendendo o JSX

--- reactjs/react-102/src/App.js
+++ reactjs/react-102/src/App.js
@@ -7,10 +7,39 @@ import React from 'react';
   }
 } */
 
+function formatarNome(usuario) {
+	return usuario.nome + ' ' + usuario.sobrenome;
+}
+
 // Forma mais usada atualmente
-/* function App() {
-  return <h1>Testando 4, 5, 6...</h1>;
-} */
+function App() {
+	let nome = 'Nathália';
+	let idade = 35;
+	let usuario = {
+		nome: 'Nathália',
+		sobrenome: 'Veneziano',
+	};
+	let imagem = 'https://www.google.com.br/google.jpg';
+
+	return (
+		// Só pode retornar 1 elemento, caso precise de mais, englobe com outras tags
+		// <div> = muito utilizada para englobar
+		// <> = fragmento que engloba o código, mas não aparce no HTML final
+		<>
+			<h1>Testando 4, 5, 6...</h1>
+
+			{/* inserindo variáveis e funções dentro de texto e atributos */}
+			<div>
+				{' '}
+				Meu nome é {nome} e tenho {idade} anos{' '}
+			</div>
+			<div> Nome Completo: {formatarNome(usuario)} </div>
+			<div> Soma de 1 + 1 = {1 + 1} </div>
+			<div> ... </div>
+			<img src={imagem} alt="Logo original do Google" />
+		</>
+	);
+}
 
 // Constante que recebe uma função (Arrow Function)
 /* let App = () => {
@@ -18,6 +47,6 @@ import React from 'react';
 } */
 
 // Arrow Function com retorno direto, pouco usado e para algo direto
-let App = () => <h1>Testando 10, 11, 12...</h1>;
+// let App = () => <h1>Testando 10, 11, 12...</h1>;
 
 export default App;
