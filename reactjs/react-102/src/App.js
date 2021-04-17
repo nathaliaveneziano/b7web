11291b6bf938f3c593ff03ed1c77c54786944718
Nathália <nathaliaveneziano@icloud.com>
2021-04-17 17:43:06 -0300

    ReactJS - Estilização com CSS e Stylesheet

--- reactjs/react-102/src/App.js
+++ reactjs/react-102/src/App.js
@@ -1,28 +1,34 @@
 import React from 'react';
-
-// Função recebendo parâmetros(props) 
-/* function BemVindo(props) {
-	return <h1>Olá {props.nome}! Você tem {props.idade} anos.</h1>;
-} */
+// Quando utilizar CSS, deve ser feita a importação do aquivo dessa forma
+// import './App.css';
 
 function Avatar(props) {
-  return <div>
-      <img src={props.user.url} alt={props.user.name}/>
-      <br/>
-      <span>{props.user.name}</span>
-    </div>
+	return (
+		// Utilizando CSS
+		// <div className="avatar">
+
+		// Utilizando StyleSheet
+		// deve ser colocado um para cada elemento
+		<div style={{ backgroundColor: '#FF0000', padding: 20, width: 150 }}>
+			<img
+				style={{ width: 50, height: 50 }}
+				src={props.user.url}
+				alt={props.user.name}
+			/>
+			<br />
+			<span>{props.user.name}</span>
+		</div>
+	);
 }
 
 function App() {
-  let user = {
-    url: 'http://www.google.com.br/google.jpg',
-    name: 'Nathália Veneziano',
-  };
+	let user = {
+		url: 'http://www.google.com.br/google.jpg',
+		name: 'Nathália Veneziano',
+	};
 
 	return (
 		<>
-			{/* <BemVindo nome="Nathália" idade="34" />
-			<BemVindo nome="Marcelo" idade ="50" /> */}
 			<Avatar user={user} />
 		</>
 	);
