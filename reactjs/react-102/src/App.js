8b627fd6a6be2491e3ac7ab55d3230d7f8a7dcf8
Nathália <nathaliaveneziano@icloud.com>
2021-04-17 17:25:53 -0300

    ReactJS - Componentes e Props

--- reactjs/react-102/src/App.js
+++ reactjs/react-102/src/App.js
@@ -1,52 +1,31 @@
 import React from 'react';
 
-// Forma muito usada até pouco tempo
-/* class App extends React.Component {
-  render() {
-    return <h1>Testando 1, 2, 3...</h1>;
-  }
+// Função recebendo parâmetros(props) 
+/* function BemVindo(props) {
+	return <h1>Olá {props.nome}! Você tem {props.idade} anos.</h1>;
 } */
 
-function formatarNome(usuario) {
-	return usuario.nome + ' ' + usuario.sobrenome;
+function Avatar(props) {
+  return <div>
+      <img src={props.user.url} alt={props.user.name}/>
+      <br/>
+      <span>{props.user.name}</span>
+    </div>
 }
 
-// Forma mais usada atualmente
 function App() {
-	let nome = 'Nathália';
-	let idade = 35;
-	let usuario = {
-		nome: 'Nathália',
-		sobrenome: 'Veneziano',
-	};
-	let imagem = 'https://www.google.com.br/google.jpg';
+  let user = {
+    url: 'http://www.google.com.br/google.jpg',
+    name: 'Nathália Veneziano',
+  };
 
 	return (
-		// Só pode retornar 1 elemento, caso precise de mais, englobe com outras tags
-		// <div> = muito utilizada para englobar
-		// <> = fragmento que engloba o código, mas não aparce no HTML final
 		<>
-			<h1>Testando 4, 5, 6...</h1>
-
-			{/* inserindo variáveis e funções dentro de texto e atributos */}
-			<div>
-				{' '}
-				Meu nome é {nome} e tenho {idade} anos{' '}
-			</div>
-			<div> Nome Completo: {formatarNome(usuario)} </div>
-			<div> Soma de 1 + 1 = {1 + 1} </div>
-			<div> ... </div>
-			<img src={imagem} alt="Logo original do Google" />
+			{/* <BemVindo nome="Nathália" idade="34" />
+			<BemVindo nome="Marcelo" idade ="50" /> */}
+			<Avatar user={user} />
 		</>
 	);
 }
-
-// Constante que recebe uma função (Arrow Function)
-/* let App = () => {
-  return <h1>Testando 7, 8, 9...</h1>;
-} */
-
-// Arrow Function com retorno direto, pouco usado e para algo direto
-// let App = () => <h1>Testando 10, 11, 12...</h1>;
 
 export default App;
