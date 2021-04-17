d84c6196e9d0afbb6e0b6c963b4f834c6ec2f03f
Nathália <nathaliaveneziano@icloud.com>
2021-04-17 18:57:14 -0300

    ReactJS - Estilização com StyledComponents (3/3)

--- reactjs/react-102/src/App.js
+++ reactjs/react-102/src/App.js
@@ -1,39 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// cria um componente já estilizado
 const Site = styled.div`
 	width: 400px;
 	height: 400px;
 	background: #00ff00;
 `;
 
-const Title = styled.h1`
-	color: #ff0000;
-	font-size: 18px;
-`;
-
 const Botao = styled.button`
 	font-size: 19px;
 	padding: 10px 15px;
-	// mudando elemento de acordo com a props
-	// background: ${(props) => props.color || '#00ff00'}
-
-	background: ${(props) => (props.ativo ? '#0000FF' : '#CCC')};
-	color: ${(props) => (props.ativo ? '#FFF' : '#000')};
+	border: 3px dashed #ff0000;
+	color: #ff0000;
+	background: #ffff;
+	margin: 5px;
+	border-radius: 5px;
 `;
 
-// Para utilizar StyledComponents, devemos instalar através do comando
-// npm install styled-components --save
+const BotaoPequeno = styled(Botao)`
+	font-size: 16px;
+	padding: 5px 10px;
+`;
+
 function App() {
 	return (
 		<Site>
-			<Title>Título bem legal</Title>
-			{/* <Botao color="#ff0000">Clique aqui</Botao> */}
-			{/* <Botao color="#0000ff">Clique aqui</Botao> */}
-			{/* <Botao>Clique aqui</Botao> */}
-			<Botao ativo={true}>Clique aqui</Botao>
-			<Botao ativo={false}>Clique aqui</Botao>
+			<Botao>Clique aqui</Botao>
+			<BotaoPequeno>Clique aqui</BotaoPequeno>
 		</Site>
 	);
 }
