3140f8b649b0a0f61335678e2ad77631ea2b50d8
Nathália <nathaliaveneziano@icloud.com>
2021-04-18 12:54:38 -0300

    ReactJS - Campo de Input

--- reactjs/react-102/src/App.js
+++ reactjs/react-102/src/App.js
@@ -1,16 +1,47 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+	width: 400px;
+	height: 30px;
+	font-size: 16px;
+	padding: 10px;
+	border: 1px solid #000;
+`;
 
 function App() {
-	const [contagem, setContagem] = useState(0);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-	const botaoAction = () => {
-		setContagem(contagem + 1);
+	// const handleEmailChange = (e) => {
+	// 	setEmail(e.target.value);
+	// };
+
+	// const handlePasswordChange = (e) => {
+	// 	setPassword(e.target.value);
+	// };
+
+	const handleButton = () => {
+		alert(`${email} - ${password}`);
 	};
 
 	return (
 		<>
-			<div>{contagem} vezes</div>
-			<button onClick={botaoAction}>Clique para aumentar</button>
+			<Input
+				placeholder="Digite um e-mail"
+				type="email"
+				value={email}
+				// onChange={handleEmailChange}
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<Input
+				placeholder="Digite uma senha"
+				type="password"
+				value={password}
+				// onChange={handlePasswordChange}
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<button onClick={handleButton}>Dizer</button>
 		</>
 	);
 }
