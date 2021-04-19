e0d39968c274160a2ebfe708c3994065e19c19e3
Nathália <nathaliaveneziano@icloud.com>
2021-04-19 18:37:50 -0300

    ReactJS - Router: Erro 404

--- reactjs/react-102/src/App.js
+++ reactjs/react-102/src/App.js
@@ -18,22 +18,21 @@ function App() {
 							<Link to="/sobre">Sobre</Link>
 						</li>
 						<li>
-							{/* Query string com único parâmetro */}
 							<Link to="/categoria?tipo=esportes">Esportes</Link>
 						</li>
 						<li>
 							<Link to="/categoria?tipo=noticias">Notícias</Link>
 						</li>
 						<li>
-							{/* Query String com múltiplos parâmetros */}
 							<Link to="/categoria?tipo=viagem&subtipo=newyork">Viagem</Link>
 						</li>
 					</ul>
 				</nav>
 			</header>
-			<hr/>
+			<hr />
 
 			<Switch>
+				{/* O Switch verifica Rota por Rota na ordem passada */}
 				<Route exact path="/">
 					<Home />
 				</Route>
@@ -43,12 +42,14 @@ function App() {
 				<Route path="/categoria">
 					<Categoria />
 				</Route>
+				{/* Por isso, a última Rota deve ser a de erro */}
+				<Route path="*">
+					<h4>Página não encontrada!</h4>
+				</Route>
 			</Switch>
 
-			<hr/>
-			<footer>
-				Todos os diretos resevados...
-			</footer>
+			<hr />
+			<footer>Todos os diretos resevados...</footer>
 		</BrowserRouter>
 	);
 }
