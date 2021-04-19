a1c13c80ffbb733bc217a58941ae8c855de33bb4
Nathália <nathaliaveneziano@icloud.com>
2021-04-19 18:48:09 -0300

    ReactJS - Router: Redirect

--- reactjs/react-102/src/App.js
+++ reactjs/react-102/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
 import Categoria from './pages/Categoria';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
@@ -16,6 +16,9 @@ function App() {
 						</li>
 						<li>
 							<Link to="/sobre">Sobre</Link>
+						</li>
+						<li>
+							<Link to="/quem-somos">Quem Somos</Link>
 						</li>
 						<li>
 							<Link to="/categoria?tipo=esportes">Esportes</Link>
@@ -38,6 +41,10 @@ function App() {
 				</Route>
 				<Route path="/sobre">
 					<Sobre />
+				</Route>
+				<Route path="/quem-somos">
+					{/* Redireciona para a rota indicada */}
+					<Redirect to="/sobre" />
 				</Route>
 				<Route path="/categoria">
 					<Categoria />
--- reactjs/react-102/src/pages/Home.js
+++ reactjs/react-102/src/pages/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Home() {
+	let history = useHistory();
+
+	const handleButton = () => {
+    // Muda o histórico para a URL indicada
+    setTimeout(() => {
+      history.replace('/sobre');
+    }, 2000);
+  };
+
 	return (
 		<div>
 			<h4>Página HOME</h4>
+			<button type="button" onClick={handleButton}>
+				Ir para a página SOBRE
+			</button>
 		</div>
 	);
 }
