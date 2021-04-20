df964931921f87a245044cfd2be04c01514b5a17
Nathália <nathaliaveneziano@icloud.com>
2021-04-20 12:48:34 -0300

    ReactJS - Redux: Connect e Dispatch

--- reactjs/react-102/src/App.js
+++ reactjs/react-102/src/App.js
@@ -1,10 +1,69 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Sobre from './pages/Sobre';
+
+// Pertence a 1º forma de acesso a URL Privada
+const isLogged = false;
+
+// Pertence a 2º forma de acesso a URL Privada
+const PrivateRoute = ({ children, ...rest }) => {
+	return (
+		// Retorna todas as props incluídas na tag
+		<Route {...rest}>
+			{/* Adiciona o componente presente dentro da tag */}
+			{isLogged ? children : <Redirect to="/login" />}
+		</Route>
+	);
+};
 
 function App() {
 	return (
-		<>
+		<BrowserRouter>
+			<header>
+				<h1>Meu Site Legal</h1>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/sobre">Sobre</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<hr />
 
-		</>
+			<Switch>
+				{/* O Switch verifica Rota por Rota na ordem passada */}
+				<Route exact path="/">
+					<Home />
+				</Route>
+
+				<Route path="/login">
+					<Login />
+				</Route>
+
+				{/* Uma forma de fazer a validação de acesso a URL
+				<Route path="/sobre">
+					{isLogged ? <Sobre /> : <Redirect to="/login" />}
+				</Route> */}
+
+				{/* Uma forma de fazer a validação de acesso a URL */}
+				<PrivateRoute path="/sobre">
+					<Sobre />
+				</PrivateRoute>
+
+				<Route path="*">
+					<h4>Página não encontrada!</h4>
+				</Route>
+			</Switch>
+
+			<hr />
+			<footer>Todos os diretos resevados...</footer>
+		</BrowserRouter>
 	);
 }
 
---
+++ reactjs/react-102/src/pages/Home.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+function Home(props) {
+	let history = useHistory();
+
+	const handleButton = () => {
+		setTimeout(() => {
+			history.replace('/sobre');
+		}, 2000);
+	};
+
+	const handleNathalia = () => {
+		props.setName('Nathália');
+	};
+
+	const handleIncrement = () => {
+		props.increment();
+	};
+
+	return (
+		<div>
+			<h4>Página HOME</h4>
+
+			<p>Nome: {props.name}</p>
+			<p>Contagem: {props.contador}</p>
+
+			<p>
+				<button type="button" onClick={handleNathalia}>
+					Setar nome para Nathália
+				</button>
+			</p>
+			<p>
+				<button type="button" onClick={handleIncrement}>
+					+1
+				</button>
+			</p>
+
+			<button type="button" onClick={handleButton}>
+				Ir para a página SOBRE
+			</button>
+		</div>
+	);
+}
+
+const mapStateToProps = (state) => {
+	return {
+		name: state.usuario.name,
+		contador: state.usuario.contador,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		setName: (newName) =>
+			dispatch({ type: 'SET_NAME', payload: { name: newName } }),
+		increment: () => 
+			dispatch({ type: 'INCREMENT_CONTADOR' }),
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
