f0fa7158a95f9005b61de18e89793b78966b9aee
Nathália <nathaliaveneziano@icloud.com>
2021-04-20 12:02:56 -0300

    ReactJS - Redux: Criando Store e Reducers

--- reactjs/react-102/src/index.js
+++ reactjs/react-102/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Reducers from './reducers';
 import App from './App';
 
+// Criando Store
+const store = createStore(Reducers);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // Ligando todo o projeto ao Store
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
 );
