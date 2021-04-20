f990dd599ee0f13940261da886da916d8aba12d8
Nathália <nathaliaveneziano@icloud.com>
2021-04-20 13:11:16 -0300

    ReactJS - Redux: Em várias telas

--- reactjs/react-102/src/pages/Login.js
+++ reactjs/react-102/src/pages/Login.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function Login() {
+function Login(props) {
+	const handleClaudia = () => {
+		props.setName('Claudia');
+	};
+
 	return (
 		<div>
 			<h4>Página LOGIN</h4>
-			<p>bla bla bla</p>
+
+			<p>Nome: {props.name}</p>
+
+			<p>
+				<button type="button" onClick={handleClaudia}>
+					Setar nome para Claudia
+				</button>
+			</p>
 		</div>
 	);
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+	name: state.usuario.name,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	setName: (newName) =>
+		dispatch({ type: 'SET_NAME', payload: { name: newName } }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
