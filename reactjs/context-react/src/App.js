676fa5c89da2a924191757daddfc27243e7d9a22
Nathália <nathaliaveneziano@icloud.com>
2021-04-21 17:39:20 -0300

    ContextAPI - Criando um Contexto

--- reactjs/context-react/src/App.js
+++ reactjs/context-react/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 
+import ThemeContext from './contexts/ThemeContext';
+
 import Header from './components/Header';
 import Menu from './components/Menu';
 import Body from './components/Body';
@@ -9,13 +11,15 @@ function App() {
 	const [userName, setUserName] = useState('Nathália');
 
 	return (
-		<div className="container">
-			<Header username={userName} />
-			<section>
-				<Menu username={userName} />
-				<Body username={userName} setUserName={setUserName} />
-			</section>
-		</div>
+		<ThemeContext.Provider value="light">
+			<div className="container">
+				<Header username={userName} />
+				<section>
+					<Menu username={userName} />
+					<Body username={userName} setUserName={setUserName} />
+				</section>
+			</div>
+		</ThemeContext.Provider>
 	);
 }
 
