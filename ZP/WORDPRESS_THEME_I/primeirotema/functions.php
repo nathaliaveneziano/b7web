8b0ef9c8c19b79ce842eda0237e50a660b040ad2
nathaliaveneziano <nathaliaveneziano@icloud.com>
2023-10-19 20:29:25 -0300

    CSS e JS com Hooks

--- ZP/WORDPRESS_THEME_I/primeirotema/functions.php
+++ ZP/WORDPRESS_THEME_I/primeirotema/functions.php
@@ -1,9 +1,9 @@
 <?php
-echo 'Me carregou';
-
-// action hooks
-function nv_fim()
+function nv_theme_styles()
 {
-  echo 'Vai encerrar tudo...';
+  wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css');
+  wp_enqueue_style('theme', get_template_directory_uri() . '/assets/css/theme.css');
+
+  wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '1.0', true);
 }
-add_action('shutdown', 'nv_fim');
+add_action('wp_enqueue_scripts', 'nv_theme_styles');
--- ZP/WORDPRESS_THEME_I/primeirotema/index.php
+++ ZP/WORDPRESS_THEME_I/primeirotema/index.php
@@ -1,1 +1,16 @@
-<h1>Meu primeiro tema</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <h1>Meu primeiro tema</h1>
+
+  <?php wp_footer(); ?>
+</body>
+
+</html>
