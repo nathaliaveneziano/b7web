9403d2f28e8697494647559a232ebe236bc6a09c
nathaliaveneziano <nathaliaveneziano@icloud.com>
2023-10-20 13:18:45 -0300

    Projeto: Tema do zero: Home

--- ZP/WORDPRESS_THEME_I/minimag/index.php
+++ ZP/WORDPRESS_THEME_I/minimag/index.php
@@ -1,9 +1,25 @@
 <?php get_header(); ?>
 
-<main class="site-main container">
-  <div class="row">
-    <section class="col-sm-8">...</section>
-    <?php get_sidebar(); ?>
+<main>
+  <div class="container">
+    <div class="row">
+      <section class="col-sm-8 main">
+        <?php
+        if (have_posts()) :
+          while (have_posts()) :
+            the_post();
+            get_template_part('template-parts/post');
+          endwhile;
+        endif;
+        ?>
+
+        <div class="pagination">
+          <div class="previous"><?php previous_posts_link(); ?></div>
+          <div class="next"><?php next_posts_link(); ?></div>
+        </div>
+      </section>
+      <?php get_sidebar(); ?>
+    </div>
   </div>
 </main>
 
