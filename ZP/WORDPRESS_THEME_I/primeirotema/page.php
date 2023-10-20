5d4b89536cd9dd96366bf5e2a6ff53730bf59256
nathaliaveneziano <nathaliaveneziano@icloud.com>
2023-10-19 21:28:32 -0300

    Página única e página não encontrada (404)

---
+++ ZP/WORDPRESS_THEME_I/primeirotema/page.php
@@ -0,0 +1,39 @@
+<?php
+get_header();
+?>
+
+<section class="site">
+  <div class="container">
+    <?php get_search_form(); ?>
+    ...
+    <?php while (have_posts()) : ?>
+      <?php the_post(); ?>
+      <article class="post">
+        <h2><?php the_title(); ?></h2>
+
+        <?php if (has_post_thumbnail()) : ?>
+          <?php the_post_thumbnail('full'); ?>
+        <?php endif; ?>
+
+        <p><?php the_content(); ?></p>
+
+        <?php if (comments_open()) : ?>
+
+          <p>
+            <?php comments_number(); ?>
+          </p>
+
+          <hr>
+        <?php
+          comments_template();
+        endif;
+        ?>
+      </article>
+    <?php endwhile; ?>
+  </div>
+
+  <?php get_sidebar(); ?>
+</section>
+
+<?php
+get_footer();
