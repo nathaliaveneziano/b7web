fc9fae0a986e8112f49c9bf4f92e8e900be9184f
nathaliaveneziano <nathaliaveneziano@icloud.com>
2023-11-07 18:29:43 -0300

    Projeto: Tema Blog B7Web (parte 2)

--- ZP/WORDPRESS_THEME_II/b7web/index.php
+++ ZP/WORDPRESS_THEME_II/b7web/index.php
@@ -2,6 +2,13 @@
 get_header();
 ?>
 
+<?php
+if (is_home()) {
+  get_template_part('template-parts/banner', 'home');
+} else {
+  get_template_part('template-parts/banner', 'single');
+}
+?>
 <main>main</main>
 
 <?php
---
+++ ZP/WORDPRESS_THEME_II/b7web/template-parts/banner-single.php
@@ -0,0 +1,3 @@
+<section class="banner">
+  banner single
+</section>
